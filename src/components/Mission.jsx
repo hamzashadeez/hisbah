e6dbc0747cb231df701e05132f598263718b4664
Sage-tex <stonestechologies@gmail.com>
2024-04-07 04:47:24 -0700

    hisba

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -39,7 +39,7 @@ function Mission() {
               href='#'
               className='mt-8 inline-block rounded bg-teal-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-teal-700 focus:outline-none focus:ring focus:ring-yellow-400'
             >
-              Get Started Today
+              Join Our Community
             </a>
           </div>
         </div>
